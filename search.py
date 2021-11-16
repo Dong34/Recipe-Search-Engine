4786da45fd77d0f78791f23b5de7487a2d0b8215
Dong34 <yierdong@umich.edu>
2021-11-16 15:19:51 -0500

    Add search.py

---
+++ search.py
@@ -0,0 +1,17 @@
+# Implement all the search-related functions
+class SearchClass():
+    def __init__(self,someVariable):
+        self.someVariable = someVariable
+
+    def someFunction(self):
+        print(self.someVariable)
+
+
+def main():
+    newObj = SearchClass("SI650")
+    newObj.someFunction()
+    print("yayyyyyy!")
+
+
+if __name__=="__main__":
+    main()
