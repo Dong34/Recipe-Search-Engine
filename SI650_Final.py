259b373658b2adaf2ec401005b01c24f47a93a49
Dong34 <yierdong@umich.edu>
2021-12-14 18:07:36 -0500

    add sub

--- SI650_Final.py
+++ SI650_Final.py
@@ -392,7 +392,7 @@ if __name__ == '__main__':
                     if ingredient_substitution_box == result_ingredients[i]:
                         ingredient_words = ingredient_substitution_box.split()
                         for word in ingredient_words:
-                            if not (word.isnumeric()) or (word in my_stop_words):
+                            if not (word.isnumeric()) or (word not in my_stop_words):
                                 substitution =  ingredient_sub(word)
                                 if substitution is not None:
                                     st.write("%s   -->   %s"%(word, substitution))
